00a8880820daaf453cdaf68944840c30e7173a73
Jade <asleepydoggy@gmail.com>
2024-10-28 03:11:24 +1100

    refactor: format main.gd
    Refactored the main script file to rename variables, clean up functions,
    and better align with Godot's style guide
    
    Fixed a bug which sometimes prevented notes from playing (and always
    prevented G#6 from playing)

--- project/mods/MidiStrummer/main.gd
+++ project/mods/MidiStrummer/main.gd
@@ -1,100 +1,94 @@
-extends Node
 class_name MidiStrummer
+extends Node
 
-#the midi value each string begins on
-var string_pitch_indices = [40,45,50,55,59,64]
+const OPEN_STRING_PITCHES := [40, 45, 50, 55, 59, 64]
+const HIGHEST_FRET := 16
 
-func pitch_is_in_bounds(pitch):
-	return pitch >= string_pitch_indices[0] or pitch <= string_pitch_indices[5] + 16
+# The timestamp and fret most recently played on each string
+var _last_strum: Array = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
+# Strings ordered from least to most recently played
+var _string_queue: Array = [0, 1, 2, 3, 4, 5]
 
-# [msec, fret]
-var last_strum = {
-	0: [0, 0],
-	1: [0, 0],
-	2: [0, 0],
-	3: [0, 0],
-	4: [0, 0],
-	5: [0, 0]
-}
+func _ready() -> void:
+	OS.open_midi_inputs()
 
-const QUEUE_MAX_SIZE = 6
-var string_queue = [0,1,2,3,4,5]
 
-func update_string_queue(last_played_string: int):
-	var last_string_index = string_queue.find(last_played_string)
-	if last_string_index != -1:
-		string_queue.pop_at(last_string_index)
-	string_queue.append(last_played_string)
-
-#returns array of possible guitar notes for a given pitch 
-#returns notes in format [string, fret]
-func pitch_to_notes(pitch):
-	if !pitch_is_in_bounds(pitch):
+func _input(event) -> void:
+	if (
+			!(event is InputEventMIDI) or
+			!(event.message == MIDI_MESSAGE_NOTE_ON and event.velocity)
+	):
 		return
 	
-	var possible_notes = []
+	var pitch: int = event.pitch
+	if !is_pitch_in_bounds(pitch):
+		return
 	
-	var string = 0
-	for index in string_pitch_indices:
-		if pitch >= index and pitch < index + 16:
-			var relative_pitch = pitch - index
-			possible_notes.append([string,relative_pitch])
+	var possible_notes := get_possible_notes(pitch)
+	var note := get_best_note(possible_notes)
+	play_note(note)
+
+
+func is_pitch_in_bounds(pitch: int) -> bool:
+	return (
+			pitch >= OPEN_STRING_PITCHES[0] and
+			pitch <= OPEN_STRING_PITCHES[5] + HIGHEST_FRET
+	)
+
+
+# returns array of possible guitar notes for a given pitch in format [string, fret]
+func get_possible_notes(pitch: int) -> Array:
+	var possible_notes := []
+	
+	var string: int = 0
+	for string_pitch in OPEN_STRING_PITCHES:
+		if pitch >= string_pitch and pitch <= string_pitch + HIGHEST_FRET:
+			var fret: int = pitch - string_pitch
+			possible_notes.append([string, fret])
 		string += 1
 	
 	return possible_notes
 
-#takes an array of notes in format [string, fret] provided by pitch_to_notes()
-#returns the note on the string which was least recently played
-#by iterating across the possible notes n seeing which is closest to the front in the string queue
-func get_best_note(notes):
-	var note_choice
-	#starts at the top of the queue
-	var least_recent_string_index = QUEUE_MAX_SIZE
+
+# takes an array of notes in format [string, fret] provided by get_possible_notes()
+# returns the note on the string which was least recently played
+# by iterating across the possible notes n seeing which is closest to the front in the string queue
+func get_best_note(notes: Array) -> Array:
+	var best_note: Array
+	# starts at the top of the queue
+	var least_recent_string_index = _string_queue.size() - 1
 	
 	for note in notes:
-		var string_index = string_queue.find(note[0])
-		if string_index < least_recent_string_index:
-			note_choice = note
+		var string_index = _string_queue.find(note[0])
+		
+		if string_index <= least_recent_string_index:
+			best_note = note
 			least_recent_string_index = string_index
 	
-	return note_choice
+	return best_note
 
 
-#takes note as [string, fret]
+# takes note as [string, fret]
 func play_note(note: Array):
-	var string = note[0]
-	var fret = note[1]
+	var string: int = note[0]
+	var fret: int = note[1]
 	
-	var delta = OS.get_system_time_msecs() - last_strum[string][0]
+	var delta: int = OS.get_system_time_msecs() - _last_strum[string][0]
 	
-	if delta < 500 and last_strum[string][1] != fret:
+	if delta < 500 and _last_strum[string][1] != fret:
 		PlayerData.emit_signal("_hammer_guitar", string, fret)
 		pass
 	else:
 		PlayerData.emit_signal("_play_guitar", string, fret, 1.0)
-		last_strum[string] = [OS.get_system_time_msecs(), fret]
+		_last_strum[string] = [OS.get_system_time_msecs(), fret]
+	
 	update_string_queue(string)
 
-func test_play(pitch):
-	var notes = pitch_to_notes(pitch)
-	var note = get_best_note(notes)
-	print("pitch: %s" % pitch)
-	print(note)
-	update_string_queue(note[0])
 
+func update_string_queue(last_played_string: int):
+	var last_string_index = _string_queue.find(last_played_string)
 
-func _ready():
-	print("MidiStrummer: _ready()")
-
-	OS.open_midi_inputs()
-
-
-func _input(event):
-	if !(event is InputEventMIDI) or !(event.message == MIDI_MESSAGE_NOTE_ON and event.velocity): return
+	if last_string_index != -1:
+		_string_queue.pop_at(last_string_index)
 	
-	var pitch = event.pitch
-	if !pitch_is_in_bounds(pitch): return
-	
-	var notes = pitch_to_notes(pitch)
-	var note = get_best_note(notes)
-	play_note(note)
+	_string_queue.append(last_played_string)
