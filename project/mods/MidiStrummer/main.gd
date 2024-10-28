ea4938796a4d7d0cbed5f57153e9c6ffb385bf44
Jade <asleepydoggy@gmail.com>
2024-10-28 23:40:54 +1100

    refactor: add leading underscores and missing types

--- project/mods/MidiStrummer/main.gd
+++ project/mods/MidiStrummer/main.gd
@@ -13,7 +13,7 @@ func _ready() -> void:
 	OS.open_midi_inputs()
 
 
-func _input(event) -> void:
+func _input(event: InputEvent) -> void:
 	if (
 			!(event is InputEventMIDI) or
 			!(event.message == MIDI_MESSAGE_NOTE_ON and event.velocity)
@@ -21,15 +21,15 @@ func _input(event) -> void:
 		return
 	
 	var pitch: int = event.pitch
-	if !is_pitch_in_bounds(pitch):
+	if !_is_pitch_in_bounds(pitch):
 		return
 	
-	var possible_notes := get_possible_notes(pitch)
-	var note := get_best_note(possible_notes)
-	play_note(note)
+	var possible_notes := _get_possible_notes(pitch)
+	var note := _get_best_note(possible_notes)
+	_play_note(note)
 
 
-func is_pitch_in_bounds(pitch: int) -> bool:
+func _is_pitch_in_bounds(pitch: int) -> bool:
 	return (
 			pitch >= OPEN_STRING_PITCHES[0] and
 			pitch <= OPEN_STRING_PITCHES[5] + HIGHEST_FRET
@@ -37,7 +37,7 @@ func is_pitch_in_bounds(pitch: int) -> bool:
 
 
 # returns array of possible guitar notes for a given pitch in format [string, fret]
-func get_possible_notes(pitch: int) -> Array:
+func _get_possible_notes(pitch: int) -> Array:
 	var possible_notes := []
 	
 	var string: int = 0
@@ -50,13 +50,13 @@ func get_possible_notes(pitch: int) -> Array:
 	return possible_notes
 
 
-# takes an array of notes in format [string, fret] provided by get_possible_notes()
+# takes an array of notes in format [string, fret] provided by _get_possible_notes()
 # returns the note on the string which was least recently played
 # by iterating across the possible notes n seeing which is closest to the front in the string queue
-func get_best_note(notes: Array) -> Array:
+func _get_best_note(notes: Array) -> Array:
 	var best_note: Array
 	# starts at the top of the queue
-	var least_recent_string_index = _string_queue.size() - 1
+	var least_recent_string_index := _string_queue.size() - 1
 	
 	for note in notes:
 		var string_index = _string_queue.find(note[0])
@@ -69,7 +69,7 @@ func get_best_note(notes: Array) -> Array:
 
 
 # takes note as [string, fret]
-func play_note(note: Array):
+func _play_note(note: Array) -> void:
 	var string: int = note[0]
 	var fret: int = note[1]
 	
@@ -82,11 +82,11 @@ func play_note(note: Array):
 		PlayerData.emit_signal("_play_guitar", string, fret, 1.0)
 		_last_strum[string] = [OS.get_system_time_msecs(), fret]
 	
-	update_string_queue(string)
+	_update_string_queue(string)
 
 
-func update_string_queue(last_played_string: int):
-	var last_string_index = _string_queue.find(last_played_string)
+func _update_string_queue(last_played_string: int) -> void:
+	var last_string_index := _string_queue.find(last_played_string)
 
 	if last_string_index != -1:
 		_string_queue.pop_at(last_string_index)
